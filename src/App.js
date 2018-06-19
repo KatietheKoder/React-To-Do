bcc0b63e1b6ee3255ce08194df7a2ba9813fdda8
KatietheKoder <katieknecht@ymail.com>
2018-06-19 19:08:30 -0400

    React To-Do: State & Props

--- src/App.js
+++ src/App.js
@@ -32,19 +32,18 @@ toggleComplete(index) {
     this.setState({ todos: todos });
   }
 
-     render() {
+  render() {
      return (
        <div className="App">
          <ul>
            { this.state.todos.map( (todo, index) =>
-             <ToDo key={ index } />
-             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ this.toggleComplete } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
            )}
          </ul>
-         <form>
-          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
-          <input type="submit" />
          <form onSubmit={ (e) => this.handleSubmit(e) }>
+           <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+           <input type="submit" />
+         </form>
        </div>
      );
    }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
  class ToDo extends Component {
    render() {
      return (
-       <li>{ this.props.description }</li>
-       <li>
-         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-         <span>{ this.props.description }</span>
-       </li>
-     );
-   }
- }
-
- export default ToDo;
+       <div>
+          <li>
+           <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+           <span>{ this.props.description }</span>
+         </li>
+       </div>
+)}}
+export default ToDo;
